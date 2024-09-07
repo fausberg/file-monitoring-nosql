2ec68cc0052996de995d0fe55064f46d861cb003
Fausberg <ivanfadeev2003@mail.ru>
2024-09-07 17:02:37 +0300

    added 2 microservices

---
+++ src/main/java/com/example/filemonitoringnosql/FileMonitoringNoSQLApplication.java
@@ -0,0 +1,13 @@
+package com.example.filemonitoringnosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileMonitoringNoSQLApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FileMonitoringNoSQLApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/filemonitoringnosql/FilemonitoringnosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filemonitoringnosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilemonitoringnosqlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
